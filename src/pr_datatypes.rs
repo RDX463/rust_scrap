397c311999a6a6a204b5273c58c750717b8e0431
RDX463 <balsarafrohan627@gmail.com>
2025-12-27 19:25:59 +0530

    Integer: Signed & Unsigned integers

--- src/pr_datatypes.rs
+++ src/pr_datatypes.rs
@@ -1,7 +1,12 @@
 fn main(){
     let x : i32 = -42;
     let y : u32 = 100;
-
     println!("Signed Integer:{}",x);
     println!("Unsigned Integer:{}",y);
+    //difference between i32(32 bits) and i64(64 bits)
+
+    let e : i32 = 2147483647;
+    let i : i64 = 9223372036854775807;
+    println!("Maximum value of i32: {}",e);
+    println!("Maximum value of i64: {}",i);
 }
