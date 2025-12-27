5a56d588809220fc7c1d12d804e18234b43af9de
RDX463 <balsarafrohan627@gmail.com>
2025-12-27 23:53:49 +0530

    CompoundDataType:tuples

--- src/cd_datatypes.rs
+++ src/cd_datatypes.rs
@@ -8,4 +8,11 @@ fn main() {
     println!("Second Fruit in Array {}",fruits[1]);
     println!("Third Fruit in Array {}",fruits[2]);
     println!("Fourth Fruit in Array {}",fruits[3]);
+//================================================================
+//Tupple Datatype
+    let human : (String , i32 , bool) = ("Rohan".to_string(),22,true); //added function to convert
+                                                                       //into String 
+    println!("My info {:?}", human);
+    let mix_tuple = ("Bats",5,false,[1,2,4,5,8]);
+    println!("Mix tuple is {:?}",mix_tuple);                //prints all datatype including array  
 }
