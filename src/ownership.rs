e0d13ef6950902840c95b570c4abdb5f8bda6824
skelking <rohanbalsaraf9@gmail.com>
2026-01-15 15:48:10 +0530

    rust: one owner at a time

--- src/ownership.rs
+++ src/ownership.rs
@@ -1,10 +1,18 @@
 //ownership,browsing and reference
 
+//fn main(){
+  //  let s1 = String::from("RUST");
+    //let len = calculate_length(&s1);
+    //println!("Length of '{}' is {}.",s1 , len);
+//}
+//fn calculate_length(s: &String) -> usize{
+  //  s.len()
+//}
+
+//there can be only one owner at a time
 fn main(){
     let s1 = String::from("RUST");
-    let len = calculate_length(&s1);
-    println!("Length of '{}' is {}.",s1 , len);
-}
-fn calculate_length(s: &String) -> usize{
-    s.len()
+    let s2 = s1;
+
+    println!("{}",s1);
 }
