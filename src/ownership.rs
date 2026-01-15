bfc9ff277c3a826bd79ce7af1898522b839da991
skelking <rohanbalsaraf9@gmail.com>
2026-01-15 15:44:50 +0530

    rust: ownership

---
+++ src/ownership.rs
@@ -0,0 +1,10 @@
+//ownership,browsing and reference
+
+fn main(){
+    let s1 = String::from("RUST");
+    let len = calculate_length(&s1);
+    println!("Length of '{}' is {}.",s1 , len);
+}
+fn calculate_length(s: &String) -> usize{
+    s.len()
+}
